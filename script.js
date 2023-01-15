f6f6233631497bfb5118210123ca3f4c0b1e630d
JulsSkogs <rjsollenskog@gmail.com>
2023-01-15 14:38:39 -0500

    add API information

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 const count = 10;
 const apiKey = config.key;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On load
+getPhotos();
